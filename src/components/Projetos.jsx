b863bd6b4e920345c923b42fcf4bbde18e3a2816
williamRibeiR0 <williamn_dev@hotmail.com>
2024-07-10 23:50:30 -0300

    ajuste de projetos

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -3,95 +3,140 @@ import img1 from "../img/calculadora.png";
 import img2 from "../img/clima.png";
 import img3 from "../img/envento_dev.png";
 import img4 from "../img/link.png";
-import img5 from "../img/jogodavelha.png"
+import img5 from "../img/jogodavelha.png";
 import img6 from "../img/snakeGame.png";
 
 function Projetos() {
     return (
-        <div className={styles.box1} id="projetos">
-            <h2>Projetos</h2>
-            <div className={styles.box2}>
-                <div >
-                    <div className={styles.box3}>
-                        <p>calculadora</p>
-                        <a
-                            href="https://williamribeir0.github.io/calculadora/"
-                            target="blank"
-                            className={styles.box4}
-                        >
-                            <img src={img1} alt="" />
-                        </a>
-                    </div>
-                    
-                    <div>
-                    <p>clima</p>
-                        <a
-                            href="https://williamribeir0.github.io/Clima/"
-                            target="blank"
-                            className={styles.box4}
-                        >
-                            <img src={img2} alt="" />
-                        </a>
+        <div className={styles.container} id="projetos">
+            <h1>Projetos</h1>
+            <div className={styles.card_container}>
+
+                <div className={styles.card_article}>
+                    <img src={img1} alt="imagem" className={styles.card_img} />
+                    <div className={styles.card_data}>
+                        <span className={styles.card_description}>
+                            HTML, CSS, JavaScript.
+                        </span>
+                        <h2 className={styles.card_title}>Calculadora</h2>
+                        <div className={styles.card_buttons}>
+                            <a href="#" className={styles.card_button}>
+                                Ver Projeto
+                            </a>
+                            <a href="#" className={styles.card_button}>
+                                Ver Code
+                            </a>
+                        </div>
                     </div>
                 </div>
 
-
-                <div >
-                    <div className={styles.box3}>
-                    <p>landing_pag</p>
-                        <a
-                            href="https://landing-pag-koea.vercel.app/"
-                            target="blank"
-                            className={styles.box4}
-                        >
-                            <img src={img3} alt="" />
-                        </a>
-                    </div>
-                    <div>
-                        <p>Dev link</p>
-                        <a
-                            href="https://dev-link-three.vercel.app/"
-                            target="blank"
-                            className={styles.box4}
-                        >
-                            <img src={img4} alt="" />
-                        </a>
+                <div className={styles.card_article}>
+                    <img src={img2} alt="imagem" className={styles.card_img} />
+                    <div className={styles.card_data}>
+                        <span className={styles.card_description}>
+                        HTML, CSS, JavaScript. 
+                        </span>
+                        <h2 className={styles.card_title}>Clima</h2>
+                        <div className={styles.card_buttons}>
+                            <a href="#" className={styles.card_button}>
+                                Ver Projeto
+                            </a>
+                            <a href="#" className={styles.card_button}>
+                                Ver Code
+                            </a>
+                        </div>
                     </div>
                 </div>
 
-                <div>
-                    <div className={styles.box3}>
-                    <p>Jogo da Velha</p>
-                        <a
-                            href="https://williamribeir0.github.io/jogo_da_velha/"
-                            target="blank"
-                            className={styles.box4}
-                        >
-                            <img src={img5} alt="" />
-                        </a>
-                    </div>
-                    <div>
-                    <p>Snake Game</p>
-                        <a
-                            href="https://snake-nu-beryl.vercel.app/"
-                            target="blank"
-                            className={styles.box4}
-                        >
-                            <img src={img6} alt="" />
-                        </a>
+                <div className={styles.card_article}>
+                    <img src={img3} alt="imagem" className={styles.card_img} />
+                    <div className={styles.card_data}>
+                        <span className={styles.card_description}>
+                        HTML, CSS, JavaScript.
+                        </span>
+                        <h2 className={styles.card_title}>Dev Link</h2>
+                        <div className={styles.card_buttons}>
+                            <a href="#" className={styles.card_button}>
+                                Ver Projeto
+                            </a>
+                            <a href="#" className={styles.card_button}>
+                                Ver Code
+                            </a>
+                        </div>
                     </div>
                 </div>
 
+                <div className={styles.card_article}>
+                    <img src={img4} alt="imagem" className={styles.card_img} />
+                    <div className={styles.card_data}>
+                        <span className={styles.card_description}>
+                        HTML, CSS, JavaScript.
+                        </span>
+                        <h2 className={styles.card_title}>Dev Link</h2>
+                        <div className={styles.card_buttons}>
+                            <a href="#" className={styles.card_button}>
+                                Ver Projeto
+                            </a>
+                            <a href="#" className={styles.card_button}>
+                                Ver Code
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className={styles.card_article}>
+                    <img src={img5} alt="imagem" className={styles.card_img} />
+                    <div className={styles.card_data}>
+                        <span className={styles.card_description}>
+                        HTML, CSS, JavaScript.
+                        </span>
+                        <h2 className={styles.card_title}>Dev Link</h2>
+                        <div className={styles.card_buttons}>
+                            <a href="#" className={styles.card_button}>
+                                Ver Projeto
+                            </a>
+                            <a href="#" className={styles.card_button}>
+                                Ver Code
+                            </a>
+                        </div>
+                    </div>
+                </div>
 
-            </div>
-            <div className={styles.titleProjetos}>
-                <a
-                    href="https://github.com/WilliamRibeiR0?tab=repositories"
-                    target="blank"
-                    className={styles.link}
-                >
-                    Ver mais Projetos
-                </a>
+                <div className={styles.card_article}>
+                    <img src={img6} alt="imagem" className={styles.card_img} />
+                    <div className={styles.card_data}>
+                        <span className={styles.card_description}>
+                        HTML, CSS, JavaScript.
+                        </span>
+                        <h2 className={styles.card_title}>Dev Link</h2>
+                        <div className={styles.card_buttons}>
+                            <a href="#" className={styles.card_button}>
+                                Ver Projeto
+                            </a>
+                            <a href="#" className={styles.card_button}>
+                                Ver Code
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.card_article}>
+                    <img src={img5} alt="imagem" className={styles.card_img} />
+                    <div className={styles.card_data}>
+                        <span className={styles.card_description}>
+                        HTML, CSS, JavaScript.
+                        </span>
+                        <h2 className={styles.card_title}>Dev Link</h2>
+                        <div className={styles.card_buttons}>
+                            <a href="#" className={styles.card_button}>
+                                Ver Projeto
+                            </a>
+                            <a href="#" className={styles.card_button}>
+                                Ver Code
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
