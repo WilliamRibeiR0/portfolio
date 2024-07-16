6173b8d6382539afee1e73b9fd0ef884e83e239e
williamRibeiR0 <williamn_dev@hotmail.com>
2024-07-15 22:21:46 -0300

    adição de links

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -1,7 +1,7 @@
 import styles from "./Projetos.module.css";
 import img1 from "../img/calculadora.png";
 import img2 from "../img/clima.png";
-import img3 from "../img/envento_dev.png";
+import img3 from "../img/image.png";
 import img4 from "../img/link.png";
 import img5 from "../img/jogodavelha.png";
 import img6 from "../img/snakeGame.png";
@@ -20,10 +20,10 @@ function Projetos() {
                         </span>
                         <h2 className={styles.card_title}>Calculadora</h2>
                         <div className={styles.card_buttons}>
-                            <a href="#" className={styles.card_button}>
+                            <a href="https://williamribeir0.github.io/calculadora/" target="blank" className={styles.card_button}>
                                 Ver Projeto
                             </a>
-                            <a href="#" className={styles.card_button}>
+                            <a href="https://github.com/WilliamRibeiR0/calculadora" target="blank" className={styles.card_button}>
                                 Ver Code
                             </a>
                         </div>
@@ -34,14 +34,14 @@ function Projetos() {
                     <img src={img2} alt="imagem" className={styles.card_img} />
                     <div className={styles.card_data}>
                         <span className={styles.card_description}>
-                        HTML, CSS, JavaScript. 
+                        HTML, CSS. 
                         </span>
                         <h2 className={styles.card_title}>Clima</h2>
                         <div className={styles.card_buttons}>
-                            <a href="#" className={styles.card_button}>
+                            <a href="https://williamribeir0.github.io/Clima/" target="blank" className={styles.card_button}>
                                 Ver Projeto
                             </a>
-                            <a href="#" className={styles.card_button}>
+                            <a href="https://github.com/WilliamRibeiR0/Clima?tab=readme-ov-file" target="blank" className={styles.card_button}>
                                 Ver Code
                             </a>
                         </div>
@@ -52,14 +52,14 @@ function Projetos() {
                     <img src={img3} alt="imagem" className={styles.card_img} />
                     <div className={styles.card_data}>
                         <span className={styles.card_description}>
-                        HTML, CSS, JavaScript.
+                        HTML, CSS.
                         </span>
-                        <h2 className={styles.card_title}>Pag Login</h2>
+                        <h2 className={styles.card_title}>Login Form</h2>
                         <div className={styles.card_buttons}>
-                            <a href="#" className={styles.card_button}>
+                            <a href="https://login-form-tan-tau.vercel.app/" target="blank" className={styles.card_button}>
                                 Ver Projeto
                             </a>
-                            <a href="#" className={styles.card_button}>
+                            <a href="https://github.com/WilliamRibeiR0/LoginForm"  target="blank" className={styles.card_button}>
                                 Ver Code
                             </a>
                         </div>
@@ -70,14 +70,14 @@ function Projetos() {
                     <img src={img4} alt="imagem" className={styles.card_img} />
                     <div className={styles.card_data}>
                         <span className={styles.card_description}>
-                        HTML, CSS, JavaScript.
+                        HTML, CSS.
                         </span>
                         <h2 className={styles.card_title}>Dev Link</h2>
                         <div className={styles.card_buttons}>
-                            <a href="#" className={styles.card_button}>
+                            <a href="https://dev-link-three.vercel.app/" target="blank" className={styles.card_button}>
                                 Ver Projeto
                             </a>
-                            <a href="#" className={styles.card_button}>
+                            <a href="https://github.com/WilliamRibeiR0/devLink" target="blank" className={styles.card_button}>
                                 Ver Code
                             </a>
                         </div>
@@ -92,10 +92,10 @@ function Projetos() {
                         </span>
                         <h2 className={styles.card_title}>Jogo Da Velha</h2>
                         <div className={styles.card_buttons}>
-                            <a href="#" className={styles.card_button}>
+                            <a href="https://williamribeir0.github.io/jogo_da_velha/" target="blank" className={styles.card_button}>
                                 Ver Projeto
                             </a>
-                            <a href="#" className={styles.card_button}>
+                            <a href="https://github.com/WilliamRibeiR0/jogo_da_velha" target="blank" className={styles.card_button}>
                                 Ver Code
                             </a>
                         </div>
@@ -110,10 +110,10 @@ function Projetos() {
                         </span>
                         <h2 className={styles.card_title}>Game Snake</h2>
                         <div className={styles.card_buttons}>
-                            <a href="#" className={styles.card_button}>
+                            <a href="https://snake-nu-beryl.vercel.app/" target="blank" className={styles.card_button}>
                                 Ver Projeto
                             </a>
-                            <a href="#" className={styles.card_button}>
+                            <a href="https://github.com/WilliamRibeiR0/snake-Game" target="blank" className={styles.card_button}>
                                 Ver Code
                             </a>
                         </div>
